703cb65c7c19087f886510210acfbe7cf8429ae0
Oaksas <aman.getnet2@gmail.com>
2021-04-01 16:45:47 -1100

    Creating the model for the registration form

--- Note/noteFiles/__init__.py
+++ Note/noteFiles/__init__.py
@@ -1,5 +1,6 @@
 from  flask import Flask
+from  noteFiles.forms import RegistrationForm 
 
 app = Flask(__name__)
-
+app.config ['SECRET_KEY'] = 'kkj32n4rkj32nrk24nda23413412er'
 from noteFiles import routes
--- Note/noteFiles/routes.py
+++ Note/noteFiles/routes.py
@@ -1,20 +1,33 @@
-from  flask import render_template
-from noteFiles import app
+from  flask import render_template, flash,redirect
+from  noteFiles import app
+from  noteFiles.forms import RegistrationForm ,LogInForm
+
+
 @app.route('/')
 @app.route('/home')
 
 def home():
     return render_template('index.html',title="index")
 
-@app.route('/')
 @app.route('/about')
-
 def about():
     return render_template('about.html',title="about")
 
-@app.route('/')
 @app.route('/account')
-
 def account():
     return render_template('account.html',title="account")
 
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created successfully for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title="register",form=form)
+    
+
+@app.route('/login')
+def login():
+    form = LogInForm()
+    return render_template('login.html',title="login",form=form)
