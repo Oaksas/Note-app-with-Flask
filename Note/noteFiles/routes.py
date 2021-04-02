f999a1426889fc15c33fa0078fd5417234063666
Oaksas <aman.getnet2@gmail.com>
2021-04-01 21:31:47 -1100

    Form verification

--- Note/noteFiles/routes.py
+++ Note/noteFiles/routes.py
@@ -1,4 +1,4 @@
-from  flask import render_template, flash,redirect
+from  flask import render_template, flash,redirect,url_for
 from  noteFiles import app
 from  noteFiles.forms import RegistrationForm ,LogInForm
 
@@ -22,12 +22,22 @@ def account():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        print('validated')
         flash(f'Account created successfully for {form.username.data}!', 'success')
         return redirect(url_for('home'))
+    else:
+        print(form.errors)
     return render_template('register.html',title="register",form=form)
     
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
     form = LogInForm()
+    if form.validate_on_submit():
+        if form.email.data == 'aman.getnet2@gmail.com' and form.password.data =='password':
+            flash(f'{form.email.data} logged in successfully','success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'login unsuccessfull, try again','danger')
+
     return render_template('login.html',title="login",form=form)
