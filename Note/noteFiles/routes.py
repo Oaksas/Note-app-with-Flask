dfb97712e6c3106b2d55d83c3b779f3f65119575
Oaksas <aman.getnet2@gmail.com>
2021-04-14 22:04:31 -1100

    Working with login and logout

--- Note/noteFiles/routes.py
+++ Note/noteFiles/routes.py
@@ -2,6 +2,7 @@ from  flask import render_template, flash,redirect,url_for
 from  noteFiles import app,db,bcrypt
 from  noteFiles.forms import RegistrationForm ,LogInForm
 from noteFiles.models import User,Notes
+from flask_login import login_user, current_user, logout_user
 
 
 @app.route('/')
@@ -21,6 +22,8 @@ def account():
 
 @app.route('/register',methods=['GET','POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit(): 
         hashedPassword=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -37,11 +40,20 @@ def register():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LogInForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password,form.password.data):
-
+            login_user(user,remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:    
             flash(f'login unsuccessfull, try again','danger')
 
     return render_template('login.html',title="login",form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
